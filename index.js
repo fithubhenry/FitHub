f158beeb243812fe8d91af5ce3e908c9c7c15b86
MatiCassagne <matiascassagne@gmail.com>
2025-08-31 20:34:35 -0300

    Modificacion index

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui.
+function laCajaDePandora(numero) {
+  if (num % 2 === 0) {
+    // Número par → convertir a binario
+    return num.toString(2);
+  } else {
+    // Número impar → convertir a hexadecimal
+    return num.toString(16);
+  }
+  // proximamente escribiremos codigo aqui.
 }
