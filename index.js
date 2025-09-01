5d07a799198bd7bbd65ce6f40a5009bc70288124
Xavier Manzoco <62972541+xavimanzoco@users.noreply.github.com>
2025-08-31 22:52:10 -0300

    modificacion funciones

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui.
+
+function laCajaDePandora(numero) {
+
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
+}
+
+function xavierManzoco() {
+    return {
+        nombre: "Xavier Manzoco",
+        edad: 29,
+        nacionalidad: "Argentina"
+    };
 }
