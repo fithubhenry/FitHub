6a98a1d40acfe4b3d389e472899cc759c280d74e
Juliana Bonet <julianabonet2@gmail.com>
2025-08-31 22:53:13 -0300

    prueba ind

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui.
+if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
+}
+
+function juli() {
+  return {
+    nombre: "Juli",
+    edad: 25,         
+    nacionalidad: "Argentina"
+  };
 }
